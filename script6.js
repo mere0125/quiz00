fe0e2b433569dc0e94ee00fb8a4b806a6eafa92b
mere0125 <157430435+mere0125@users.noreply.github.com>
2024-04-23 00:16:08 -0400

    Add files via upload

---
+++ script6.js
@@ -0,0 +1,30 @@
+var choices = [];
+
+function choose(choice) {
+    choices.push(choice);
+    showNextQuestion();
+}
+
+function showNextQuestion() {
+    var questions = document.getElementById("buttonContainer");
+    var resultContainer = document.getElementById("resultContainer");
+
+    if (choices.length === 2) {
+        questions.style.display = "none";
+        resultContainer.style.display = "block";
+        var resultText = "Your choices: " + choices.join(", ");
+        resultContainer.textContent = resultText;
+    }
+}
+
+var leftButton = document.getElementById("leftButton");
+
+leftButton.addEventListener("click", function() {
+    window.location.href = "question7.html";
+});
+
+var rightButton = document.getElementById("rightButton");
+
+rightButton.addEventListener("click", function() {
+    window.location.href = "question7.html";
+});
